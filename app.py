5f1a8ab043436c49e3c509f8b54b0e4848401539
Pasha Dwimahendra <dwimahendrapashax@gmail.com>
2019-12-09 14:28:00 +0700

    flask get data

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+API_URL = 'http://localhost:8000/api/v1'
+
+@app.route('/')
+def index():
+    jobs = requests.get(f'{API_URL}/job/')
+    jobs = jobs.json()
+    return render_template('index.html', jobs=jobs)
